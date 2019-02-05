d5eb517a3b66f975d82e5d747fb30fb4fa0a90f2
Thomas Großbeck <grossbec@hm.edu>
2019-02-05 15:46:27 +0100

    ControllerTests hinzugefügt

---
+++ 3.3/src/test/java/edu/hm/ba/classic/controller/BookControllerTest.java
@@ -0,0 +1,152 @@
+package edu.hm.ba.classic.controller;
+
+import edu.hm.ba.classic.entities.Book;
+import edu.hm.ba.classic.entities.Category;
+import edu.hm.ba.classic.services.BookService;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookController.class)
+@WebAppConfiguration
+public class BookControllerTest {
+
+    private static final Collection<Book> BOOKS = new ArrayList<>(Arrays.asList(
+            new Book("1234567890000", "TestBuch1", "TestAutor", Category.SCIENCE),
+            new Book("1234567890001", "TestBuch2", "TestAutor", Category.FANTASY),
+            new Book("1234567890002", "TestBuch3", "Autor", Category.HISTORY)
+    ));
+
+    private static final Book BOOK = new Book("1234567890000", "TestBuch1", "TestAutor", Category.SCIENCE);
+
+    @MockBean
+    private BookService bookService;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    public void testGetBooks() throws Exception {
+        when(bookService.getBooks()).thenReturn(BOOKS);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/books");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("[{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\",\"lender\":null},{\"isbn\":\"1234567890001\",\"title\":\"TestBuch2\",\"author\":\"TestAutor\",\"category\":\"FANTASY\",\"lender\":null},{\"isbn\":\"1234567890002\",\"title\":\"TestBuch3\",\"author\":\"Autor\",\"category\":\"HISTORY\",\"lender\":null}]",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "EMPLOYEE")
+    public void testAddBook() throws Exception {
+        when(bookService.addBook(BOOK)).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\"}");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\",\"lender\":null}",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CUSTOMER")
+    public void testAddBookAccessDenied() throws Exception {
+        exception.expectCause(IsInstanceOf.<Throwable>instanceOf(AccessDeniedException.class));
+        when(bookService.addBook(BOOK)).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\"}");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    }
+
+    @Test
+    @WithMockUser(authorities = "EMPLOYEE")
+    public void testUpdateBook() throws Exception {
+        when(bookService.updateBook(BOOK.getIsbn(), BOOK)).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/book/1234567890000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\"}");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\",\"lender\":null}",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CUSTOMER")
+    public void testUpdateBookAccessDenied() throws Exception {
+        exception.expectCause(IsInstanceOf.<Throwable>instanceOf(AccessDeniedException.class));
+        when(bookService.updateBook(BOOK.getIsbn(), BOOK)).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/book/1234567890000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\"}");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    }
+
+    @Test
+    @WithMockUser(authorities = "EMPLOYEE")
+    public void testDeleteBook() throws Exception {
+        when(bookService.deleteBook(BOOK.getIsbn())).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/book/1234567890000");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("{\"isbn\":\"1234567890000\",\"title\":\"TestBuch1\",\"author\":\"TestAutor\",\"category\":\"SCIENCE\",\"lender\":null}",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CUSTOMER")
+    public void testDeleteBookAccessDenied() throws Exception {
+        exception.expectCause(IsInstanceOf.<Throwable>instanceOf(AccessDeniedException.class));
+        when(bookService.deleteBook(BOOK.getIsbn())).thenReturn(BOOK);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/book/1234567890000");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    }
+
+    @Test
+    public void testLendBook() {
+
+    }
+
+    @Test
+    public void testReturnBook() {
+
+    }
+
+}
---
+++ 3.3/src/test/java/edu/hm/ba/classic/controller/StatisticControllerTest.java
@@ -0,0 +1,107 @@
+package edu.hm.ba.classic.controller;
+
+import edu.hm.ba.classic.entities.Category;
+import edu.hm.ba.classic.entities.Statistic;
+import edu.hm.ba.classic.services.StatisticService;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StatisticController.class)
+@WebAppConfiguration
+public class StatisticControllerTest {
+
+    private static final Collection<Statistic> STATISTICS = new ArrayList<>(Arrays.asList(
+            new Statistic(1, 23, Category.SCIENCE),
+            new Statistic(1, 12, Category.HISTORY),
+            new Statistic(1, 55, Category.FANTASY)
+    ));
+
+    private static final Statistic STATISTIC = new Statistic(1, 34, Category.SCIENCE);
+
+    @MockBean
+    private StatisticService statisticService;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    public void testCount() {
+
+    }
+
+    @Test
+    @WithMockUser(authorities = "EMPLOYEE")
+    public void testGetStatistics() throws Exception {
+        when(statisticService.getStatistics()).thenReturn(STATISTICS);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/statistics");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("[{\"id\":1,\"count\":23,\"category\":\"SCIENCE\"},{\"id\":1,\"count\":12,\"category\":\"HISTORY\"},{\"id\":1,\"count\":55,\"category\":\"FANTASY\"}]",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CUSTOMER")
+    public void testGetStatisticsAccessDenied() throws Exception {
+        exception.expectCause(IsInstanceOf.<Throwable>instanceOf(AccessDeniedException.class));
+        when(statisticService.getStatistics()).thenReturn(STATISTICS);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/statistics");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    }
+
+    @Test
+    @WithMockUser(authorities = "EMPLOYEE")
+    public void testGetStatistic() throws Exception {
+        when(statisticService.getStatistic("SCIENCE")).thenReturn(STATISTIC);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/statistic/SCIENCE");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("{\"id\":1,\"count\":34,\"category\":\"SCIENCE\"}", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CUSTOMER")
+    public void testGetStatisticAccessDenied() throws Exception {
+        exception.expectCause(IsInstanceOf.<Throwable>instanceOf(AccessDeniedException.class));
+        when(statisticService.getStatistic("SCIENCE")).thenReturn(STATISTIC);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/statistic/SCIENCE");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    }
+
+}
