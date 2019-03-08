559401bdfc082fe11d7955063e5c6d58de4edf01
Thomas Großbeck <grossbec@hm.edu>
2019-03-08 09:38:17 +0100

    README angepasst

--- 3.4/src/test/java/edu/hm/ba/classic/controller/BookControllerIntegrationTest.java
+++ 3.4/src/test/java/edu/hm/ba/classic/controller/BookControllerIntegrationTest.java
@@ -72,32 +72,4 @@ public class BookControllerIntegrationTest {
         assertEquals("{\"isbn\":\"9783764504458\",\"title\":\"Blackout\",\"author\":\"Marc Elsberg\",\"category\":\"FANTASY\",\"lender\":null}",
                 result.getResponse().getContentAsString());
     }
-/*
-    @Test
-    public void testLendBook() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/lend/9783764504458");
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        assertEquals(200, result.getResponse().getStatus());
-        assertEquals("{\"id\":\"1\",\"username\":\"Admin\",\"password\":\"admin1\"}",
-                result.getResponse().getContentAsString());
-    }
-
-    @Test
-    public void testReturnBook() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/return/9783764504458");
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        assertEquals(200, result.getResponse().getStatus());
-        assertEquals("{\"id\":\"1\",\"username\":\"Admin\",\"password\":\"admin1\"}",
-                result.getResponse().getContentAsString());
-    }
-
-    @Test
-    public void testGetLendings() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/lend");
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        assertEquals(200, result.getResponse().getStatus());
-        assertEquals("[{\"isbn\":\"9783442151479\",\"title\":\"Bildung - Alles, was man wissen muss\",\"author\":\"Dietrich Schwanitz\",\"category\":\"SCIENCE\",\"lender\":null},{\"isbn\":\"9783806234770\",\"title\":\"Schwarze Flaggen\",\"author\":\"Joby Warrick\",\"category\":\"HISTORY\",\"lender\":null},{\"isbn\":\"9783764504458\",\"title\":\"Blackout\",\"author\":\"Marc Elsberg\",\"category\":\"FANTASY\",\"lender\":null}]",
-                result.getResponse().getContentAsString());
-    }
-*/
 }
--- README.md
+++ README.md
@@ -1,34 +1,13 @@
-# bachelorarbeit
+# Sinnvolle Einsatzmöglichkeiten und Umsetzungsstrategien für serverless Webanwendungen
 
-## Sinnvolle Einsatzmöglichkeiten und Umsetzungsstrategien für serverless Webanwendungen
+### 3.4
 
-1. Einleitung und Motivation
-2. Grundlagen der Serverless-Architektur
-<br>2.1 Historische Entwicklung des Cloud-Computing
-<br>&nbsp;&nbsp;2.1.1 Grundlagen des Cloud-Computing
-<br>&nbsp;&nbsp;2.1.2 Abgrenzung zu PaaS
-<br>&nbsp;&nbsp;2.1.3 Abgrenzung zu Microservices
-<br>2.2 Eigenschaften von Function-as-a-Service
-<br>2.3 Allgemeine Patterns für Serverless-Umsetzungen
-<br>&nbsp;&nbsp;2.3.1 Serverless Computing Manifest
-<br>&nbsp;&nbsp;2.3.2 Schnittstellen zu anderen Architekturen
-3. Vergleich zweier prototypischen Anwendung
-<br>3.1 Vorgehensweise beim Vergleich der beiden Anwendungen
-<br>3.2 Fachliche Beschreibung der Beispiel-Anwendung
-<br>3.3 Implementierung der Benutzeroberfläche
-<br>3.4 Implementierung der klassischen Webanwendung
-<br>&nbsp;&nbsp;3.4.1 Architektonischer Aufbau der Applikation
-<br>&nbsp;&nbsp;3.4.2 Implementierung der Anwendung
-<br>&nbsp;&nbsp;3.4.3 Testen der Webanwendung
-<br>3.5 Implementierung der serverless Webanwendung
-<br>&nbsp;&nbsp;3.5.1 Architektonischer Aufbau der Serverless-Applikation
-<br>&nbsp;&nbsp;3.5.2 Implementierung der Anwendung
-<br>&nbsp;&nbsp;3.5.3 Testen von serverless Anwendungen
-<br>3.6 Unterschiede in der Entwicklung
-<br>&nbsp;&nbsp;3.6.1 Durchführung der Evaluation
-<br>&nbsp;&nbsp;3.6.2 Auswertung der Evaluation
-4. Vergleich der beiden Umsetzungen
-<br>4.1 Vorteile der Serverless-Infrastruktur
-<br>4.2 Nachteile der Serverless-Infrastruktur
-<br>4.3 Abwägung sinnvoller Einsatzmöglichkeiten
-5. Fazit und Ausblick
+Code der klassischen Webanwendung. Enthält unter /src/main/resources/static die Implementierung des Frontends.
+
+### 3.5
+
+Code der Serverless Webanwendung. Nutzt das Frontend aus 3.4.
+
+### 3.6
+
+Ergebnisse der Evaluation
